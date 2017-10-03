fccd7b3ea750d1747df2322bfaf3892415a2d492
DivineBrad <bradleymjblanchard@gmail.com>
2017-10-03 16:05:57 -0400

    Test Server with code to process file upload  transcode

--- server/test.js
+++ server/test.js
@@ -1,38 +1,100 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const formidable = require('formidable');
+const bodyParser = require('body-parser');
+const formidable=require('formidable');
+const ffmpeg=require('fluent-ffmpeg');
 const publicPath = path.join(__dirname, '../public');
 const adminPath = path.join(__dirname, '../public/admin');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
+
+
+var { mongoose } = require('./db/mongoose');
+var { Channel } = require('./models/channel');
+var { Mediafile } = require('./models/mediafile');
 
 var app = express();
 
 app.set('view engine', 'hbs');
+
 app.use(express.static(publicPath));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    
+
 });
 
-app.get('/admin', (req, res) => {
-    res.sendFile(adminPath + '/index.html');
-});
+// Form Upload 
+
 app.get('/formupload', (req, res) => {
     res.sendFile(adminPath + '/test.html');
 });
 app.post('/fileupload', (req, res) => {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/xampp/htdocs/KuunganaTv-Prototype/public/media/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
+      //var absoluteAppPath='C:\\xampp\\htdocs\\KuunganaTv-Prototype\\';
+      
+      var oldPath = files.filetoupload.path;
+      
+      var mediaFolder = publicPath+"/media/";
+      fs.existsSync(mediaFolder) || fs.mkdirSync(mediaFolder);
+      var mediaFile=files.filetoupload.name;
+  
+      var fileNameExt=path.parse(mediaFile);
+      
+      var fileName=fileNameExt.name;
+      
+      var fileFolder=mediaFolder+"/"+fileName+'/';
+     fs.existsSync(fileFolder) || fs.mkdirSync(fileFolder);
+     var originalFolder=fileFolder+'/original/';
+     var hlsFolder=fileFolder+'/hls/';
+ 
+      fs.existsSync(originalFolder) || fs.mkdirSync(originalFolder);
+      fs.existsSync(hlsFolder) || fs.mkdirSync(hlsFolder);
+      
+        //Insert Logic to create media db doc with title name and use that name for folder name.
+        // Name must be unique
+
+
+        ///End of Insert Logic
+
+      
+
+      var newPath = originalFolder +'/'+files.filetoupload.name;
+      fs.rename(oldPath, newPath, function (err) {
         if (err) throw err;
-        res.write('File uploaded and moved!');
+        var command=ffmpeg(newPath);
+        command
+        // set hls segments time
+        .addOption('-hls_time', 3)
+        // include all the segments in the list
+        .addOption('-hls_list_size',0)
+        // start number
+        .addOption('-start_number', 1)
+        // setup event handlers
+        .on('end', function() {
+          console.log('file has been converted succesfully');
+        })
+        .on('error', function(err) {
+          console.log('an error happened: ' + err.message);
+        })
+        // save to file
+        .save(hlsFolder+fileName+'.m3u8');
+            
+            res.write('File uploaded and moved!');
+    
+       
+
         res.end();
       });
  });
 });
 
+// Api 
+
+app.get('/admin', (req, res) => {
+    res.sendFile(adminPath + '/index.html');
+});
 
 app.get('/business', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'video/mp4' });
@@ -44,6 +106,19 @@ app.get('/politics', (req, res) => {
     var stream = fs.createReadStream(__dirname + '/media/R.mp4');
     stream.pipe(res);
 });
+
+app.post('/channels', (req, res) => {
+    var channel = new Channel({
+        name: req.body.name,
+        description: req.body.description
+    });
+    channel.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+    console.log(req.body);
+});
 app.listen(port, () => {
-    console.log(`server is up on port 5000`);
+    console.log(`server is up on port 4000`);
 });
