b0c8e87a85b17fae9469d338c646e8976d38bfca
ajibdeji <ajibdeji@gmail.com>
2017-10-30 20:19:39 -0400

    re commit

--- public/admin/js/angular/controllers/channel-controller.js
+++ public/admin/js/angular/controllers/channel-controller.js
@@ -1,4 +1,6 @@
-App.controller('channel-controller', function($scope, $http) {
+/* global app */
+
+app.controller('channel-controller', function($scope, $http) {
     $scope.postChannel = function() {
         $http({
             method: "POST",
@@ -9,8 +11,23 @@ App.controller('channel-controller', function($scope, $http) {
             }
         }).then(function(response) {
             console.log(response.data);
+            $scope.name="";
+            $scope.description="";
         }, function(response) {
             console.log(response.statusText);
         });
-    }
+    };
+    $scope.getChannels = function() {
+        $http({
+            method: "GET",
+            url: "http://localhost:4000/channels",
+            data: {}
+        }).then(function(response) {
+            $scope.allChannels=response.data.channels;
+            console.log($scope.allChannels);
+            
+        }, function(response) {
+            console.log(response.statusText);
+        });
+    };
 });
--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
+/* global process, __dirname */
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const publicPath = path.join(__dirname, '../public');
 const adminPath = path.join(__dirname, '../public/admin');
@@ -10,6 +13,7 @@ const port = process.env.PORT || 4000;
 var { mongoose } = require('./db/mongoose');
 var { Channel } = require('./models/channel');
 var { Mediafile } = require('./models/mediafile');
+var {ObjectID}=require('mongodb');
 
 var app = express();
 
@@ -18,6 +22,25 @@ app.set('view engine', 'hbs');
 app.use(express.static(publicPath));
 app.use(bodyParser.json());
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.get('/', (req, res) => {
 
 });
@@ -68,6 +91,39 @@ app.post('/channels', (req, res) => {
         res.status(400).send(e);
     });
     console.log(req.body);
+});
+
+app.get('/channels', (req, res) => {
+   Channel.find().then((channels)=>{
+      res.send({channels});
+      console.log(channels);
+   }, (e)=>{
+      res.status(400).send(e); 
+   });
+    console.log(req.body);
+});
+
+app.patch('/channels/:id', (req, res) => {
+    var id = req.params.id;
+    var body=_.pick(req.body, ['name', 'description']);
+
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send({message: "Invalid Object Id"});
+    }
+
+    body.name=req.body.name;
+    body.description=req.body.description;
+
+    Channel.findByIdAndUpdate(id, {$set:body}, {new:true}).then((channel)=>{
+        if (!channel){
+            return res.status(404).send({message: "Channel doesn't exist"});
+        }  
+        res.send({channel});
+        
+    }).catch((e)=>{
+        res.status(400).send({ message: "Error Occured",e}); 
+    });
+        console.log(req.body);
 });
 app.listen(port, () => {
     console.log(`server is up on port 4000`);
